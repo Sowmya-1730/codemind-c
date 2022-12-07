ca143baa386baeb5ef4aa63ed4e895b435e6e4ff
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2022-12-07 11:00:45 +0530

    #include<stdio.h> int main() {   int n,s=0,r,q;   scanf("%d",&n);   q=n;   while(q!=0)   {       r=q%10;       q=q/10;       s=s*10+r;   }   if(s==n)   {       printf("Palindrome");   }   else   {       printf("Not Palindrome");   } }

--- Palindrome.c
+++ Palindrome.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,s=0,r,q;
-    scanf("%d",&n);
-    q=n;
-    while(q!=0)
-    {
-        r=q%10;
-        q=q/10;
-        s=s*10+r;
-    }
-    if(n==s)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
+  int n,s=0,r,q;
+  scanf("%d",&n);
+  q=n;
+  while(q!=0)
+  {
+      r=q%10;
+      q=q/10;
+      s=s*10+r;
+  }
+  if(s==n)
+  {
+      printf("Palindrome");
+  }
+  else
+  {
+      printf("Not Palindrome");
+  }
 }
