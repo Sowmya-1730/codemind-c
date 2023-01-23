d7b874a9414d1f29587e6661e24f94cebfc1d950
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2023-01-23 18:59:04 +0530

    #include<stdio.h> int main() {     int a,b,i,j,sum=0;     scanf("%d%d",&a,&b);     int arr[a][b];     for(i=0;i<a;i++)     {         for(j=0;j<b;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<a;i++)     {         for(j=0;j<b;j++)         {             sum=sum+arr[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_matrix_elements.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j,sum=0;
+    scanf("%d%d",&a,&b);
+    int arr[a][b];
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            sum=sum+arr[i][j];
+        }
+    }   
+    printf("%d",sum);
+}
